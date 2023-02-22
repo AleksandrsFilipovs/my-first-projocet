7fc566eb3b146c0f8f2b213fa02bbbaed4fdee83
AleksandrsFilipovs <sanfill29@gmail.com>
2023-02-22 20:37:16 +0200

    [main]: Add new print line

--- src/main/java/lv/acodemy/Main.java
+++ src/main/java/lv/acodemy/Main.java
@@ -3,5 +3,6 @@ package lv.acodemy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my name is Aleks. This is my new project. ");
+        System.out.println("Hello world");
     }
 }
