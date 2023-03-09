fea0f52b4551c9761e9c0668211d61cdd3e1c678
AleksandrsFilipovs <sanfill29@gmail.com>
2023-03-09 20:49:49 +0200

    [classroom_09032023]: Add classroom progress

--- src/main/java/lv/acodemy/classroom/VariablesDataTypes.java
+++ src/main/java/lv/acodemy/classroom/VariablesDataTypes.java
@@ -40,7 +40,7 @@ public class VariablesDataTypes {
         System.out.printf("%s %s %s\n", isSpring, isWinter, hasApples);
 
         // string
-        String name = "Alex";
+        String name = "Aleksandrs";
         String surName = "Filipovs";
 
         // concatenation
@@ -87,5 +87,30 @@ public class VariablesDataTypes {
                 System.out.println("This is even numbers: " + num);
             }
         }
+    }
+}
+
+
+
+class Main {
+    public static void main(String[] args) {
+
+        // declare variables
+        int a = 12, b = 5;
+
+        // addition operator
+        System.out.println("a + b = " + (a + b));
+
+        // subtraction operator
+        System.out.println("a - b = " + (a - b));
+
+        // multiplication operator
+        System.out.println("a * b = " + (a * b));
+
+        // division operator
+        System.out.println("a / b = " + (a / b));
+
+        // modulo operator
+        System.out.println("a % b = " + (a % b));
     }
 }
